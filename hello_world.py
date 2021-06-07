817a86194035274eb40af6b59714b6c8c295e7a5
Hayley van Waas <hello@hayley.codes>
2021-06-07 19:51:29 +0800

    Create hello_world.py
    Intro to variabels.

---
+++ hello_world.py
@@ -0,0 +1,34 @@
+print("Hello, Hayley!")
+
+# day = "Monday"
+# print(type(day))
+# month = "June"
+# print(day)
+# print(f"Today is: {day}, and the month is {month}.")
+# this is a comment
+
+# create a variable for the current month
+# output this in the same sentence
+
+run1_dist = 1400
+print(run1_dist)
+print(type(run1_dist))
+run2_dist = 1800
+
+total_distance = run1_dist + run2_dist
+difference = run2_dist - run1_dist
+print(total_distance)
+print(difference)
+
+run3_dist = 1.7
+run4_dist = 1.35
+print(type(run4_dist))
+
+print(run1_dist / 1000)
+print(run4_dist * 1000)
+
+run5_dist = "5000"
+print(run5_dist + "3")
+print(int(run5_dist) * 3)
+# print(run5_dist * 3.0)
+print(type(str(run4_dist)))
